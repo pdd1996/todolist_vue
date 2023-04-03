dfcbea2b30f3cad4bde34ac80af3602f2a162864
paddy <pdd337816464@163.com>
2023-04-03 22:58:56 +0800

    添加 - grid布局

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,49 @@
 <template>
-  <div class="container">
-    <div class="page_background">
-      <div class="top_bar">
-        <div>
+  <el-row :gutter="20">
+    <el-col :span="4">
+      <div class="grid-content bg-purple"></div>
+    </el-col>
+    <el-col :span="16">
+      <el-container>
+        <el-header>
           <span class="today">我的一天</span>
           <span class="time" id="time">时间</span>
-        </div>
-      </div>
-      <div class="content">
-        <!--显示-->
-        <ul class="list"></ul>
-      </div>
-      <div class="bottom_bar">
-        <form>
-          <label>
-            <input type="text" placeholder="添加任务" />
-            <button type="submit">添加</button>
-          </label>
-        </form>
-      </div>
-    </div>
-  </div>
+        </el-header>
+        <el-main>
+          <el-table
+            :data="tableData"
+            style="width: 100%">
+            <el-table-column
+              prop="date"
+              label="日期"
+              width="180">
+            </el-table-column>
+          </el-table>
+        </el-main>
+        <el-footer>
+          <el-input v-model="input" placeholder="添加todo项"></el-input>
+          <el-button type="primary" icon="el-icon-plus" circle></el-button>
+        </el-footer>
+      </el-container>
+    </el-col>
+    <el-col :span="4">
+      <div class="grid-content bg-purple"></div>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: "Home",
-  components: {},
+  data() {
+    return {
+      tableData: []
+    }
+  }
 };
 </script>
 
-<style>
-body {
-  min-height: 100%;
-  position: relative;
-}
+<style scoped lang="less">
 
 .container {
   height: 100vh;
@@ -50,126 +57,9 @@ body {
   /* z-index: 100; */
 }
 
-.top_bar {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 30%;
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
 }
 
-.top_bar div {
-  padding-top: 5%;
-  color: #fff;
-}
-
-.top_bar .today {
-  font-size: 64px;
-  margin: 0 0 0 20%;
-}
-
-.top_bar .time {
-  font-size: 32px;
-  display: block;
-  margin: 10px 10% 0 20%;
-}
-.content {
-  position: absolute;
-  top: 30%;
-  left: 0;
-  right: 0;
-  height: 50%;
-}
-
-.content ul {
-  width: 60%;
-  margin: 30px auto;
-  list-style: none;
-  padding: 0;
-}
-
-.content ul li {
-  background-color: #f2f2f2;
-  margin: 10px 0;
-  border-radius: 5px;
-  font-size: 28px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-}
-
-.content ul li input {
-  width: 40px;
-  height: 40px;
-  border-radius: 10px;
-}
-
-.content ul li span {
-  flex: 1;
-  margin-left: 22px;
-}
-
-.content ul li button {
-  background-color: #e74c3c;
-  color: #fff;
-  border: none;
-  padding: 5px 10px;
-  border-radius: 5px;
-  font-size: 16px;
-  cursor: pointer;
-  margin-left: 10px;
-}
-
-.content ul li.completed span {
-  text-decoration: line-through;
-  color: #aaa;
-}
-
-.content ul li.completed button {
-  background-color: #ccc;
-}
-
-.bottom_bar {
-  position: absolute;
-  top: 75%;
-  left: 0;
-  right: 0;
-  height: 25%;
-}
-
-.bottom_bar form {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.bottom_bar form label {
-  width: 60%;
-  margin: 0 auto;
-}
-
-.bottom_bar form input {
-  width: 80%;
-  min-width: 750px;
-  height: 80px;
-  padding: 10px 20px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  font-size: 32px;
-}
-
-.bottom_bar form button {
-  width: 100px;
-  height: 60px;
-  margin-left: 20px;
-  background-color: #e74c3c;
-  color: #fff;
-  border: none;
-  padding: 5px 10px;
-  border-radius: 10px;
-  font-size: 28px;
-  cursor: pointer;
-}
 </style>
